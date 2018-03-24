f841dc28ae16ea73e9f57c0a10a725c951e78d8e
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-25 01:59:10 +0700

    Header Compoent.

---
+++ firetest/src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style/header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+      <h1 className="header">Hello , Welcome to Whatsite ??</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
