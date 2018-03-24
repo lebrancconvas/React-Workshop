e6e2ba437dc651b4729b0182fd690f6c69f4d2f4
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-24 15:36:22 +0700

    :+1:Create Content Component FIle.

---
+++ firetest/src/content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Content extends Component {
+  render(){
+    return(
+      <div>
+        <p className="App-Content">เว็บนี้รวบรวมความฮาและความขำขันเอาไว้จากทั่วราชอาณาจักร</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
