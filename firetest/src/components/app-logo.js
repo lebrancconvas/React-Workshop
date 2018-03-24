daa94055c0d26b4e2ce70a87ab7592d9e6a43160
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-25 01:58:32 +0700

    App-Logo Component.

---
+++ firetest/src/components/app-logo.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './style/app-logo.css'
+
+class Applogo extends Component {
+  render() {
+    return (
+        <div>
+          <div className="column-padd">
+            <div className="columns">
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+              <div className="column has-text-centered">
+                <img src="/img/PacGosu.png" className="App-logo" alt="logo" />
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default Applogo;
