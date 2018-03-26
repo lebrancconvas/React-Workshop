b380f21af9777ea0c127b229d341c1f28db58307
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-26 16:19:12 +0700

    Edit import App location && Import React-Router && Create React-Router Component.

--- firetest/src/index.js
+++ firetest/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/App';
 import 'bulma/css/bulma.css';
 import registerServiceWorker from './registerServiceWorker';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppWithRouter = () => (
+  <Router>
+    <App />
+  </Router>
+)
+
+
+ReactDOM.render(<AppWithRouter />, document.getElementById('root'));
 registerServiceWorker();
